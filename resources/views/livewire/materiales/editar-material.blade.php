72081a0ab7d2e4a68033aac8df4cb28f395257cd
angeles <piotroskiangeles@gmail.com>
2024-10-14 19:55:37 -0300

    se commitea el composer.lock, package-lock.json y archivos de config y publicaciones

---
+++ resources/views/livewire/materiales/editar-material.blade.php
@@ -0,0 +1,31 @@
+<div>
+    <x-modal-default title="Editar Material: {{ $model->nombre }}" name="Editar-Material">
+        <x-slot:body>
+            <div class="p-4">
+                <form wire:submit.prevent="editarMaterial({{ $model->id }})" class="gap-3 d-flex flex-column">
+                    <div class="gap-2 d-flex flex-column form-group">
+                        <label for="nombre">Nombre</label>
+                        <x-input type="text" wire:model="nombre" id="nombre" class="form-control" />
+
+                        @error('nombre')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="gap-2 d-flex flex-column form-group">
+                        <label for="precioNormal">Precio Normal</label>
+                        <x-input type="text" wire:model="precioNormal" id="precioNormal" class="form-control" />
+
+                        @error('precioNormal')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="gap-3 d-flex justify-content-end">
+                        <x-button type="submit" class="btn btn-primary">
+                            Guardar
+                        </x-button>
+                    </div>
+                </form>
+            </div>
+        </x-slot:body>
+    </x-modal-default>
+</div>
